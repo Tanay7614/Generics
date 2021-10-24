8d03bb9c6fc8205e2d8be0616fe121a84ba0027f
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-24 21:13:42 +0530

    Third Taste case

--- MaxNo3.java
+++ MaxNo3.java
@@ -6,11 +6,15 @@ public class MaxNo3 {
 		System.out.println("Maximum="+max);
 	}
 
-	private static Integer maxofthree(Integer i, Integer j, Integer k) {
+	private static Integer maxofthree(Integer i, Integer j, Integer k) 
+	{
 		Integer max=i;
-		if(j.compareTo(i)>0) {
+		if(j.compareTo(i)>0)
+		{
 			max=j;
-		}else if(k.compareTo(j)>0) {
+		}
+		else if(k.compareTo(j)>0)
+		{
 			max=k;
 		}
 		return max;
